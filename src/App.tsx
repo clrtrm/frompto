282e8e7652e4313202bed35c3443fe918a844fb3
clrtrm <75531970+clrtrm@users.noreply.github.com>
2026-07-22 15:45:49 +0200

    refactor: move routes from main.tsx to App.tsx for better SRP

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import Layout from "./layouts";
+import { Routes, Route } from 'react-router-dom'
+import Layout from "./layouts"
+import Login from './pages/Login.tsx'
+import { ProtectedRoute } from './components/ProtectedRoute.tsx'
 
 function App() {
-  return (<Layout />)
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/auth/AuthProvider.tsx'
 import './index.css'
 import App from './App.tsx'
-import Login from './pages/Login.tsx'
-import { ProtectedRoute } from './components/ProtectedRoute.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <App />
-              </ProtectedRoute>
-            } />
-        </Routes>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,

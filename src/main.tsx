c822953294000507b664edd5ebc74f11813457e6
clrtrm <75531970+clrtrm@users.noreply.github.com>
2026-07-26 20:11:05 +0200

    refactor: clean imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import AuthProvider from '~/context/auth/AuthProvider.tsx'
-import App from './App.tsx'
+import AuthProvider from '~/context/auth/AuthProvider'
+import App from './App'
 
 import '@fontsource/space-grotesk/400.css'
 import '@fontsource/space-grotesk/500.css'

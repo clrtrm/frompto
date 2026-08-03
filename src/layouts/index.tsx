3a1715292dffa1beba9373a0ee006b66cf19c6bc
clrtrm <75531970+clrtrm@users.noreply.github.com>
2026-08-03 14:28:17 +0200

    feat: use useDocumentTitle in default page layout as fallback

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import type { ReactElement } from 'react'
 import NavBar from '~/components/NavBar'
+import useDocumentTitle from '~/hooks/useDocumentTitle'
 
 import './styles.scss'
 
 const Layout = (): ReactElement => {
+  useDocumentTitle('Prompto: Cozy Check-ins with Strangers')
+
   return (
     <div className="layout">
       <NavBar />

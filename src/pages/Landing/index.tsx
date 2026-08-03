3e2d6328243f485aac3c63b21c153406ba3bd896
clrtrm <75531970+clrtrm@users.noreply.github.com>
2026-08-03 14:27:48 +0200

    feat: style Landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,20 @@
 import type { ReactElement } from 'react'
 
+import './styles.scss'
+
 const LandingPage = (): ReactElement => {
   return (
-    <div>
-      <h1>Landing</h1>
+    <div className="page landing-page">
+      <div className="title-and-pitch">
+        <h1 className="page-title">Prompto</h1>
+        <span className="app-pitch">
+          New question, every morning, 10am sharp.
+          <br />
+          You've got 24 hours to answer before the big reveal.
+          <br />
+          Show up daily, get a little less stranger-y.
+        </span>
+      </div>
     </div>
   )
 }

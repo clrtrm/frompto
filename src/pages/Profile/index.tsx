2912b1fca08c6e2d72ec1b3a5261d630b4f48c4f
clrtrm <75531970+clrtrm@users.noreply.github.com>
2026-08-10 11:40:10 +0200

    feat: create Profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,13 +1,38 @@
+import PageTitle from '~/components/PageTitle'
+import useAuth from '~/context/auth/useAuth'
 import useDocumentTitle from '~/hooks/useDocumentTitle'
 
 import type { ReactElement } from 'react'
 
+import './styles.scss'
+
 const ProfilePage = (): ReactElement => {
   useDocumentTitle('Profile')
 
+  const { user } = useAuth()
+
+  /** Render */
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile-page">
+      <PageTitle textContent="Profile" />
+      <div className="page-main-content">
+        <div className="user-info">
+          <div className="user-info__image" />
+          <div className="user-info__text">
+            <span className="user-info-key">Username:</span>
+            <span>{user?.username}</span>
+
+            <span className="user-info-key">Display name:</span>
+            {user?.displayName ? (
+              <span className="user-info-value">{user.displayName}</span>
+            ) : (
+              <span className="user-info-value--default">
+                No display name defined
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
